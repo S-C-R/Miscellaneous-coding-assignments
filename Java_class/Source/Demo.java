47ac45236a18eee4cb9144e074a666a8f6f31fe7
SCR <115115778+S-C-R@users.noreply.github.com>
2026-07-09 20:54:17 -0500

    Add files via upload

---
+++ Java_class/Source/Demo.java
@@ -0,0 +1,8 @@
+//package code;
+public class Demo {
+	public static void main(String[] args){
+		System.out.println("\n\tThis is my first java program:\n\t\tMy name is Ben, I paid the fees\n");
+		System.out.println("\t\tWelcome to Java!\n");
+		System.out.println("\tThis HW shows that \n\t\tI have the resources \n\t\tto write, compile, and execute Java programs\n");
+	}
+}
